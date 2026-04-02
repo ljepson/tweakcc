74f9322236077788900969d846918b81a05d20a3
Lonny Jepson <jiskiras@gmail.com>
2026-04-01 20:35:17 -0600

    wip: preserve context collapse patch stub

---
+++ src/patches/contextCollapse.ts
@@ -0,0 +1,8 @@
+// Please see the note about writing patches in ./index
+
+import { showDiff } from "./index";
+
+export const writeContextCollapse = (oldFile: string): string | null => {
+  // TODO: implement context collapse patch
+  return oldFile;
+};
